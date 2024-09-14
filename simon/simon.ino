a84473f360968d7bf81e39929a9a05eec8029fc3
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-14 14:53:57 -0700

    mapped button to light up corresponding LED when it is pressed

--- simon/simon.ino
+++ simon/simon.ino
@@ -1,18 +1,28 @@
-int LEDout = 12;
 int btn1In = 2;
+int LEDout1 = 8;
+
 int btn2In = 7;
+int LEDout2 = 12;
 
 void setup() {
-    pinMode(LEDout, OUTPUT);
+    pinMode(LEDout1, OUTPUT);
+    pinMode(LEDout2, OUTPUT);
     pinMode(btn1In, INPUT);
     pinMode(btn2In, INPUT);
 }
 
 void loop() {
-  if(digitalRead(btn1In) == HIGH || digitalRead(btn2In) == HIGH) {
-    digitalWrite(LEDout, HIGH);
+  if(digitalRead(btn1In) == HIGH) {
+    digitalWrite(LEDout1, HIGH);
   }
   else {
-    digitalWrite(LEDout, LOW);
+    digitalWrite(LEDout1, LOW);
+  }
+
+  if(digitalRead(btn2In) == HIGH) {
+    digitalWrite(LEDout2, HIGH);
+  }
+  else {
+    digitalWrite(LEDout2, LOW);
   }
 }
