d8584563622b2c83bb83a58dce10ef6a55be347c
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-14 17:54:33 -0700

    refactored snippet that detects button click using blink method

--- simon/simon.ino
+++ simon/simon.ino
@@ -16,30 +16,26 @@ void setup() {
 
 void loop() {
   // //random LED BLINK logic
-  randNumber = random(2);
-  if(randNumber == 0) {
-    blink(grBtnOutp);
-  }
-  else {
-    blink(redBtnOutp);
-  }
-  delay(500);
+  // randNumber = random(2);
+  // if(randNumber == 0) {
+  //   blink(grBtnOutp, 1000);
+  // }
+  // else {
+  //   blink(redBtnOutp, 1000);
+  // }
+  // delay(500);
   // //random LED BLINK logic
 
-  // if(digitalRead(grBtnInp) == HIGH) {
-  //   digitalWrite(grBtnOutp, HIGH);
-  //   delay(500);
-  //   digitalWrite(grBtnOutp, LOW);
-  // }
-  // else if(digitalRead(redBtnInp) == HIGH) {
-  //   digitalWrite(redBtnOutp, HIGH);
-  //   delay(500);
-  //   digitalWrite(redBtnOutp, LOW);
-  // }
+  if(digitalRead(grBtnInp) == HIGH) {
+    blink(grBtnOutp, 50);
+  }
+  else if(digitalRead(redBtnInp) == HIGH) {
+    blink(redBtnOutp, 50);
+  }
 }
 
-void blink(int outputPin) {
+void blink(int outputPin, int delayInt) {
   digitalWrite(outputPin, HIGH);
-  delay(1000);
+  delay(delayInt);
   digitalWrite(outputPin, LOW);
 }
