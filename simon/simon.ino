b9235814d4741ce60808014a47b86309e2cb07b2
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-14 17:25:45 -0700

    logic for random LED to blink and detecting button click work independently. Need to combine the functionalities together

--- simon/simon.ino
+++ simon/simon.ino
@@ -16,36 +16,30 @@ void setup() {
 
 void loop() {
   // //random LED BLINK logic
-  // randNumber = random(2);
-  // if(randNumber == 0) {
-  //   digitalWrite(grBtnOutp, HIGH);
-  //   delay(1000);
-  //   digitalWrite(grBtnOutp, LOW);
-  // }
-  // else {
-  //   digitalWrite(redBtnOutp, HIGH);
-  //   delay(1000);
-  //   digitalWrite(redBtnOutp, LOW);
-  // }
-  // delay(500);
-  // //random LED BLINK logic
-
-  //  // Wait for user input
-  // while (Serial.available() == 0) {
-  //   // Do nothing (wait until input is available)
-  // }
-  // userInput = Serial.parseInt();
-
-  if(digitalRead(grBtnInp) == HIGH) {
+  randNumber = random(2);
+  if(randNumber == 0) {
     digitalWrite(grBtnOutp, HIGH);
-    delay(500);
+    delay(1000);
     digitalWrite(grBtnOutp, LOW);
   }
-  else if(digitalRead(redBtnInp) == HIGH) {
+  else {
     digitalWrite(redBtnOutp, HIGH);
-    delay(500);
+    delay(1000);
     digitalWrite(redBtnOutp, LOW);
   }
+  delay(500);
+  // //random LED BLINK logic
+
+  // if(digitalRead(grBtnInp) == HIGH) {
+  //   digitalWrite(grBtnOutp, HIGH);
+  //   delay(500);
+  //   digitalWrite(grBtnOutp, LOW);
+  // }
+  // else if(digitalRead(redBtnInp) == HIGH) {
+  //   digitalWrite(redBtnOutp, HIGH);
+  //   delay(500);
+  //   digitalWrite(redBtnOutp, LOW);
+  // }
 }
 
 void btnToLEDmap(int inputPin, int outputPin) {
