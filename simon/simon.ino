4af1afa91105e1837f0e1d1228829b859e41cece
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-17 16:05:57 -0700

    Game of Simon with 2 LEDs and game output reporting to Serial Monitor (#11)
    * base .ino file contains logic for the LED to turn on while the button is being pressed
    
    * modified project folder hierarchy
    
    * connected 2nd button to breadboard and gave it the same functionality as button 1
    
    * mapped button to light up corresponding LED when it is pressed
    
    * refactored variable names
    
    * refactored button to LED mapping logic to a function
    
    * arduino is turning on a random LED in a forever loop
    
    * refactored button click to LED light up logic
    
    * logic for random LED to blink and detecting button click work independently. Need to combine the functionalities together
    
    * refactored: code surrounding random LED blink logic. Created a blink method
    
    * refactored snippet that detects button click using blink method
    
    * board waits for green button click, then prints it to Serial Monitor
    
    * refactored: migrated variables and setup from commented out snippet to running code
    
    * messaged is displayed in Serial Monitor ONLY after a button is pressed AND released
    
    * code reports to Serial Monitor which button (red OR green) has been pressed and released
    
    * corresponding LED is lit after user clicks button
    
    * removed unnecessary code
    
    * removed more unnecessary code
    
    * program reports whether LED chosen matches the random LED presented by the game
    
    * initializing LEDchoices array in loop method
    
    * refactored 2nd parameter for blink invoking instances for a smoother flow of the game
    
    * score is reported to Serial Monitor after choosing an LED option (0 OR 1)
    
    * score is being kept for the game, even going past the first round. However, rounds are NOT cumulative patterns
    
    * if user chooses incorrect LED, set score back to 0
    
    * tracking correct LED choices in an array with dynamically allocated memory
    
    * added to README.md
    
    * modified README.md
    
    * changes LEDchoices array to a String
    
    * variable name changes
    
    * initialized HashTable with LED color as key (string) and corresponding output pin var as value (int)
    
    * updated README.md
    
    * fixed bug that displayed extra LED at the start of rounds 3 or higher
    
    * refactored blink method and renamed other method to validChoice
    
    * refactored snippet to break during appropriate conditions
    
    * program requires user to enter previous LEDs + new one
    
    * refactor 1: adjusted comments and deleted unnecessary code
    
    * adjusted indenting
    
    * refactored snippet that details random LED blink logic
    
    * wrote a routine to signal user the end of the current game
    
    * if user chooses incorrect LED, the game ends right then and there
    
    * removed unused method
    
    * declared and instantiated gameOver method
    
    * accurately reporting Highest score
    
    * reporting score AND highest (of current sesson) to Serial Monitor
    
    * refactored: hoisted score variable up to the global level, to prevent passing it down through multiple layers of methods
    
    * updated README.md
    
    * refactored code, LEDchoices is now an int array and LEDchoice is an int
    
    * replaced LEDchoice with LEDoutput for more maintainability in the future
    
    * removed code related to Hashtable library

---
+++ simon/simon.ino
@@ -0,0 +1,122 @@
+long randNumber;
+int highScore;
+int score;
+
+int grBtnInp = 2;
+int grBtnOutp = 8;
+
+int redBtnInp = 7;
+int redBtnOutp = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(grBtnOutp, OUTPUT);
+  pinMode(redBtnOutp, OUTPUT);
+  pinMode(grBtnInp, INPUT);
+  pinMode(redBtnInp, INPUT);
+
+  delay(1000);
+}
+
+void loop() {
+  score = 0;
+  int* LEDoutputs = new int[1];
+
+  while (true) {
+    int LEDoutput;
+
+    //random LED BLINK logic
+    randNumber = random(2);
+    if(randNumber == 0) LEDoutput = grBtnOutp;
+    else LEDoutput = redBtnOutp;
+    //random LED BLINK logic
+
+    //Current game LEDs are lit
+    LEDoutputs[score] = LEDoutput;
+
+    for(int i = 0; i < score + 1; i++) 
+    {
+      blink(LEDoutputs[i], 600);
+    }
+    //Current game LEDs are lit
+
+    //User response
+    for(int i = 0; i < score + 1; i++) 
+    {
+      // Wait until button is clicked is available
+      while (digitalRead(grBtnInp) == LOW && digitalRead(redBtnInp) == LOW) {
+        // Do nothing, just wait
+      }
+        
+      if(digitalRead(grBtnInp) == HIGH)
+      {
+        blink(grBtnOutp, 300);
+        if(LEDoutputs[i] != grBtnOutp)
+        {
+          gameOver();
+          return;
+        }         
+      }
+      else  
+      {
+        blink(redBtnOutp, 300);
+        if(LEDoutputs[i] != redBtnOutp) 
+        {
+          gameOver();
+          return;
+        } 
+      }
+        
+      while (digitalRead(grBtnInp) == HIGH || digitalRead(redBtnInp) == HIGH) {
+        // Do nothing, just wait
+      }
+    }
+    //User response
+    
+    score++;
+    reportScore();
+
+    //modify array by dynamically allocating memory
+    int newLength = score + 1;
+    int* LEDoutputsTmp = new int[newLength];
+
+    for(int i = 0; i < newLength - 1; i++) LEDoutputsTmp[i] = LEDoutputs[i];
+
+    LEDoutputsTmp[newLength - 1] = LEDoutput;
+
+    delete[] LEDoutputs;
+    LEDoutputs = LEDoutputsTmp;
+    //modify array by dynamically allocating memory
+  }
+}
+
+void gameOver() {
+    reportScore();
+
+    for(int i = 0; i < 3; i++){
+    digitalWrite(grBtnOutp, HIGH);
+    digitalWrite(redBtnOutp, HIGH);
+    delay(200);
+    digitalWrite(grBtnOutp, LOW);
+    digitalWrite(redBtnOutp, LOW);
+    delay(200);
+  }
+  delay(1000);
+}
+
+void reportScore() {
+  Serial.print("Score: ");
+  Serial.println(score);
+
+  if(score > highScore) highScore = score;
+
+  Serial.print("Highest: ");
+  Serial.println(highScore);
+}
+
+void blink(int outputPin, int delayInt) {
+  digitalWrite(outputPin, HIGH);
+  delay(delayInt);
+  digitalWrite(outputPin, LOW);
+  delay(delayInt);
+}
