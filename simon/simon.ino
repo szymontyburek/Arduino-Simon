a1668c43f6621876508307730ec11b6e0d3940f1
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-24 14:16:12 -0700

    brought in code from 4 digit segment display example into this project

--- simon/simon.ino
+++ simon/simon.ino
@@ -1,147 +1,204 @@
-long randNumber;
-int highScore;
-int score;
+// long randNumber;
+// int highScore;
+// int score;
 
-int grBtnInp = 2;
-int grBtnOutp = 7;
+// int grBtnInp = 2;
+// int grBtnOutp = 7;
 
-int redBtnInp = 3;
-int redBtnOutp = 8;
+// int redBtnInp = 3;
+// int redBtnOutp = 8;
 
-int ylBtnInp = 4;
-int ylBtnOutp = 9;
+// int ylBtnInp = 4;
+// int ylBtnOutp = 9;
 
-int blBtnInp = 5;
-int blBtnOutp = 10;
+// int blBtnInp = 5;
+// int blBtnOutp = 10;
+
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(grBtnInp, INPUT);
+//   pinMode(grBtnOutp, OUTPUT);
+//   pinMode(redBtnInp, INPUT);
+//   pinMode(redBtnOutp, OUTPUT);
+//   pinMode(ylBtnInp, INPUT);
+//   pinMode(ylBtnOutp, OUTPUT);
+//   pinMode(blBtnInp, INPUT);
+//   pinMode(blBtnOutp, OUTPUT);
+
+//   delay(1000);
+// }
+
+// void loop() {
+//   score = 0;
+//   int* LEDoutputs = new int[1];
+
+//   while (true) {
+//     int LEDoutput;
+
+//     //random LED BLINK logic
+//     randNumber = random(4);
+//     if(randNumber == 0) LEDoutput = grBtnOutp;
+//     else if (randNumber == 1) LEDoutput = redBtnOutp;
+//     else if(randNumber == 2) LEDoutput = ylBtnOutp;
+//     else LEDoutput = blBtnOutp;
+//     //random LED BLINK logic
+
+//     //Current game LEDs are lit
+//     LEDoutputs[score] = LEDoutput;
+
+//     for(int i = 0; i < score + 1; i++) 
+//     {
+//       blink(LEDoutputs[i], 600);
+//     }
+//     //Current game LEDs are lit
+
+//     //User response
+//     for(int i = 0; i < score + 1; i++) 
+//     {
+//       // Wait until button is clicked is available
+//       while (digitalRead(grBtnInp) == LOW && digitalRead(redBtnInp) == LOW && digitalRead(ylBtnInp) == LOW && digitalRead(blBtnInp) == LOW) {
+//         // Do nothing, just wait
+//       }
+        
+//       int requiredPin = LEDoutputs[i];
+
+//       if(digitalRead(grBtnInp) == HIGH)
+//       {
+//         if(!validInput(grBtnOutp, requiredPin)) return;       
+//       }
+//       else if(digitalRead(redBtnInp) == HIGH)
+//       {
+//         if(!validInput(redBtnOutp, requiredPin)) return;       
+//       }
+//       else if(digitalRead(ylBtnInp) == HIGH) {
+//         if(!validInput(ylBtnOutp, requiredPin)) return;       
+//       }
+//       else {
+//         if(!validInput(blBtnOutp, requiredPin)) return;       
+//       }
+        
+//       while (digitalRead(grBtnInp) == HIGH || digitalRead(redBtnInp) == HIGH || digitalRead(ylBtnInp) == HIGH || digitalRead(blBtnInp) == HIGH) {
+//         // Do nothing, just wait
+//       }
+//     }
+//     //User response
+    
+//     delay(300);
+//     score++;
+//     reportScore();
+
+//     //modify array by dynamically allocating memory
+//     int newLength = score + 1;
+//     int* LEDoutputsTmp = new int[newLength];
+
+//     for(int i = 0; i < newLength - 1; i++) LEDoutputsTmp[i] = LEDoutputs[i];
+
+//     LEDoutputsTmp[newLength - 1] = LEDoutput;
+
+//     delete[] LEDoutputs;
+//     LEDoutputs = LEDoutputsTmp;
+//     //modify array by dynamically allocating memory
+//   }
+// }
+
+// bool validInput(int chosenPin, int requiredPin) {
+//   blink(chosenPin, 150);
+//   if(chosenPin != requiredPin)
+//   {
+//     gameOver();
+//     return false;
+//   }  
+//   return true;
+// }
+
+// void gameOver() {
+//     reportScore();
+
+//     for(int i = 0; i < 3; i++){
+//     digitalWrite(grBtnOutp, HIGH);
+//     digitalWrite(redBtnOutp, HIGH);
+//     digitalWrite(ylBtnOutp, HIGH);
+//     digitalWrite(blBtnOutp, HIGH);
+//     delay(200);
+//     digitalWrite(grBtnOutp, LOW);
+//     digitalWrite(redBtnOutp, LOW);
+//     digitalWrite(ylBtnOutp, LOW);
+//     digitalWrite(blBtnOutp, LOW);
+//     delay(200);
+//   }
+//   delay(1000);
+// }
+
+// void reportScore() {
+//   Serial.print("Score: ");
+//   Serial.println(score);
+
+//   if(score > highScore) highScore = score;
+
+//   Serial.print("Highest: ");
+//   Serial.println(highScore);
+// }
+
+// void blink(int outputPin, int delayInt) {
+//   digitalWrite(outputPin, HIGH);
+//   delay(delayInt);
+//   digitalWrite(outputPin, LOW);
+//   delay(delayInt / 2);
+// }
+
+//www.elegoo.com
+//2016.12.12
+
+int latch=12;  //orange
+int clock=13; //green
+int data=11;   //yellow
+
+unsigned char table[]=
+{0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c
+,0x39,0x5e,0x79,0x71,0x00};
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(grBtnInp, INPUT);
-  pinMode(grBtnOutp, OUTPUT);
-  pinMode(redBtnInp, INPUT);
-  pinMode(redBtnOutp, OUTPUT);
-  pinMode(ylBtnInp, INPUT);
-  pinMode(ylBtnOutp, OUTPUT);
-  pinMode(blBtnInp, INPUT);
-  pinMode(blBtnOutp, OUTPUT);
-
-  delay(1000);
+  pinMode(latch,OUTPUT);
+  pinMode(clock,OUTPUT);
+  pinMode(data,OUTPUT);
 }
+void Display(unsigned char num)
+{
 
+  digitalWrite(latch,LOW);
+  shiftOut(data,clock,MSBFIRST,table[num]);
+  digitalWrite(latch,HIGH);
+  
+}
 void loop() {
-  score = 0;
-  int* LEDoutputs = new int[1];
-
-  while (true) {
-    int LEDoutput;
-
-    //random LED BLINK logic
-    randNumber = random(4);
-    if(randNumber == 0) LEDoutput = grBtnOutp;
-    else if (randNumber == 1) LEDoutput = redBtnOutp;
-    else if(randNumber == 2) LEDoutput = ylBtnOutp;
-    else LEDoutput = blBtnOutp;
-    //random LED BLINK logic
-
-    //Current game LEDs are lit
-    LEDoutputs[score] = LEDoutput;
-
-    for(int i = 0; i < score + 1; i++) 
-    {
-      blink(LEDoutputs[i], 600);
-    }
-    //Current game LEDs are lit
-
-    //User response
-    for(int i = 0; i < score + 1; i++) 
-    {
-      // Wait until button is clicked is available
-      while (digitalRead(grBtnInp) == LOW && digitalRead(redBtnInp) == LOW && digitalRead(ylBtnInp) == LOW && digitalRead(blBtnInp) == LOW) {
-        // Do nothing, just wait
-      }
-        
-      int requiredPin = LEDoutputs[i];
-
-      if(digitalRead(grBtnInp) == HIGH)
-      {
-        if(!validInput(grBtnOutp, requiredPin)) return;       
-      }
-      else if(digitalRead(redBtnInp) == HIGH)
-      {
-        if(!validInput(redBtnOutp, requiredPin)) return;       
-      }
-      else if(digitalRead(ylBtnInp) == HIGH) {
-        if(!validInput(ylBtnOutp, requiredPin)) return;       
-      }
-      else {
-        if(!validInput(blBtnOutp, requiredPin)) return;       
-      }
-        
-      while (digitalRead(grBtnInp) == HIGH || digitalRead(redBtnInp) == HIGH || digitalRead(ylBtnInp) == HIGH || digitalRead(blBtnInp) == HIGH) {
-        // Do nothing, just wait
-      }
-    }
-    //User response
-    
-    delay(300);
-    score++;
-    reportScore();
-
-    //modify array by dynamically allocating memory
-    int newLength = score + 1;
-    int* LEDoutputsTmp = new int[newLength];
-
-    for(int i = 0; i < newLength - 1; i++) LEDoutputsTmp[i] = LEDoutputs[i];
-
-    LEDoutputsTmp[newLength - 1] = LEDoutput;
-
-    delete[] LEDoutputs;
-    LEDoutputs = LEDoutputsTmp;
-    //modify array by dynamically allocating memory
-  }
-}
-
-bool validInput(int chosenPin, int requiredPin) {
-  blink(chosenPin, 150);
-  if(chosenPin != requiredPin)
-  {
-    gameOver();
-    return false;
-  }  
-  return true;
-}
-
-void gameOver() {
-    reportScore();
-
-    for(int i = 0; i < 3; i++){
-    digitalWrite(grBtnOutp, HIGH);
-    digitalWrite(redBtnOutp, HIGH);
-    digitalWrite(ylBtnOutp, HIGH);
-    digitalWrite(blBtnOutp, HIGH);
-    delay(200);
-    digitalWrite(grBtnOutp, LOW);
-    digitalWrite(redBtnOutp, LOW);
-    digitalWrite(ylBtnOutp, LOW);
-    digitalWrite(blBtnOutp, LOW);
-    delay(200);
-  }
-  delay(1000);
-}
-
-void reportScore() {
-  Serial.print("Score: ");
-  Serial.println(score);
-
-  if(score > highScore) highScore = score;
-
-  Serial.print("Highest: ");
-  Serial.println(highScore);
-}
-
-void blink(int outputPin, int delayInt) {
-  digitalWrite(outputPin, HIGH);
-  delay(delayInt);
-  digitalWrite(outputPin, LOW);
-  delay(delayInt / 2);
+  Display(1);
+  delay(5000);
+  Display(2);
+  delay(5000);
+  Display(3);
+  delay(5000);
+  Display(4);
+  delay(5000);
+  Display(5);
+  delay(5000);
+  Display(6);
+  delay(5000);
+  Display(7);
+  delay(5000);
+  Display(8);
+  delay(5000);
+  Display(9);
+  delay(5000);
+  Display(10);
+  delay(500);
+  Display(11);
+  delay(500);
+  Display(12);
+  delay(500);
+  Display(13);
+  delay(500);
+  Display(14);
+  delay(500);
+  Display(15);
+  delay(500);
 }
